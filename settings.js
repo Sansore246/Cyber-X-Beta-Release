030a6c7209511068e70e415cbcd6bad47b42ae36
Sansore246 <115323443+Sansore246@users.noreply.github.com>
2022-10-15 02:41:59 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,17 +36,17 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
+global.vcardowner = ['201126554338'] //ur owner number
+global.ownername = "𝐴 𝑌 𝐴 𝑇 𝑂 <𝐴𝑆𝑆𝑇>" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.owner = ['201126554338']
+global.ownertag = '201126554338' //ur tag number
+global.botnumber = '201126554338' //Add your Bot Number For Log
+global.botname = '𝐴 𝑌 𝐴 𝑇 𝑂 𝐵 𝑂 𝑇' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
